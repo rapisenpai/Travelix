a795517d78389fc4714b11f7ffe280835f522d22
Rafhael Lowix A. Homwad <86836550+rapisenpai@users.noreply.github.com>
2022-07-20 12:05:25 +0800

    Add files via upload

---
+++ rapisenpai3/functions.js
@@ -0,0 +1,33 @@
+// Close Button
+
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
+// Back To Top Button
+let mybutton = document.getElementById("back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
